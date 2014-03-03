0fafa12e2db3be92a78a31f00f35c29111549f0f
Dowen <terryden5566@yahoo.com.tw>
2014-03-04 00:51:32 +0800

    initial_of_map_and_ball

--- game4.8/Source/mygame.h
+++ game4.8/Source/mygame.h
@@ -159,11 +159,20 @@ public:
 	CGameMap();
 	void LoadBitmap();
 	void OnShow();
+
+	void OnMove();
+	void OnKeyDown(UINT);
+	void RandomBouncingBall();
+	void InitializeBouncingBall(int,int,int);
+	~CGameMap();
 protected:
 	CMovingBitmap blue,green;
 	int map[4][5];
 	const int x,y;
 	const int mw,mh;
+
+	CBouncingBall *bballs;
+	int random_num;
 };
 
 /////////////////////////////////////////////////////////////////////////////
